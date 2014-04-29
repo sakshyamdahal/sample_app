78e3f69e6786681709b5582ff5d4eaa2a004d1df
sakshyamdahal <dahalsakshyam@gmail.com>
2014-04-29 16:57:38 -0700

    Use base title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,12 @@ describe "Static pages" do
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title("#{base_title} | Home")
+			expect(page).to have_title("#{base_title}")
+		end
+
+		it "should not have custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to have_title('| Home')
 		end
 	end
 
