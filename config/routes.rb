c35a3ead2ebe309d47524f6c7951f467bc41bc40
sakshyamdahal <dahalsakshyam@gmail.com>
2014-05-02 18:01:56 -0700

    Bug fix 2

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ SampleApp::Application.routes.draw do
 
 
   root 'static_pages#home'
-  match '/signup', to: 'user#new', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
